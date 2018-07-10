5e5c41d7164529cf4c3c39fff05a6f810c201625
eruizgar91 <enrique@oceanprotocol.com>
2018-07-10 10:33:43 +0200

    Update documentation

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst', encoding='utf-8') as readme_file:
 with open('HISTORY.rst', encoding='utf-8') as history_file:
     history = history_file.read()
 
-requirements = ['oceandb-driver-interface', ]
+requirements = ['oceandb-driver-interface', 'bigchaindb_driver', 'BigchainDB', ]
 
 setup_requirements = ['pytest-runner', ]
 
